34b5719fcfe1e397796f4408f5b6253a90608ff9
aajungroth <aajungroth@ucdavis.edu>
2019-10-06 19:57:38 -0700

    Create the character controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CharacterController } from './character/character.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CharacterController],
   providers: [AppService],
 })
 export class AppModule {}
