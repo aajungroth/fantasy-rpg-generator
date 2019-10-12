27dc40f5790074e8aa6b3cb3b6b10cc0f0d6c746
aajungroth <aajungroth@ucdavis.edu>
2019-10-11 21:03:51 -0700

    Adjust spacing

--- src/character/character.controller.ts
+++ src/character/character.controller.ts
@@ -39,36 +39,31 @@ export class CharacterController {
 
   // Get the requested ancestry if the character has that ancestry
   @Get(':id/ancestry/:ancestryID')
-  public findOneAncestry(@Param() params):
-    string {
+  public findOneAncestry(@Param() params): string {
       return params;
   }
 
   // Get all of the character's backgounds
   @Get(':id/background')
-  public findAllBackgounds(@Param() params):
-    string {
+  public findAllBackgounds(@Param() params): string {
       return params;
    }
 
   // Get the requested background if the character has that background
   @Get(':id/background/:backgroundID')
-  public findOneBackound(@Param() params):
-    string {
+  public findOneBackound(@Param() params): string {
       return params;
   }
 
   // Get all of the character's classes
   @Get(':id/class')
-  public findAllClasses(@Param() params):
-    string {
+  public findAllClasses(@Param() params): string {
       return params;
   }
 
   // Get the requested class if the character has that class
   @Get(':id/class/:classID')
-  public findOneClass(@Param() params):
-    string {
+  public findOneClass(@Param() params): string {
       return params;
    }
 
