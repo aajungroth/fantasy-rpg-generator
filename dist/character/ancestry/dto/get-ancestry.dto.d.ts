e70c970f4d36d0260352ef0ad45b3c5338ac582e
aajungroth <aajungroth@ucdavis.edu>
2020-02-03 21:23:01 -0800

    Add build for the ancestry API

---
+++ dist/character/ancestry/dto/get-ancestry.dto.d.ts
@@ -0,0 +1,4 @@
+export declare class GetAncestryDto {
+    readonly id: number;
+    readonly name: string;
+}
---
+++ dist/character/ancestry/dto/get-ancestry.dto.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class GetAncestryDto {
+}
+exports.GetAncestryDto = GetAncestryDto;
+//# sourceMappingURL=get-ancestry.dto.js.map
