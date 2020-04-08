c63c81e31d0318191b895b7579e56cace544489b
aajungroth <aajungroth@ucdavis.edu>
2020-04-08 12:02:33 -0700

    Add character dao

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharacterController } from './character/character.controller';
 import { CharacterService } from './character/character.service';
+import { CharacterDao } from './character/character.dao';
 
 @Module({
   imports: [],
   controllers: [AppController, CharacterController],
-  providers: [AppService, CharacterService],
+  providers: [AppService, CharacterService, CharacterDao],
 })
 export class AppModule {}
