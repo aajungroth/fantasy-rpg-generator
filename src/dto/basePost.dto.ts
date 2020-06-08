64eae0c9fa470c7771c42d5776a61fd0ce138030
aajungroth <aajungroth@ucdavis.edu>
2020-06-07 17:26:11 -0700

    Add base post dto to handle all post requests from games

---
+++ src/dto/basePost.dto.ts
@@ -0,0 +1,7 @@
+export class BasePostDto {
+  readonly name: string;
+  readonly description: string;
+  readonly createdBy: string;
+  readonly lastChangedBy: string;
+  readonly internalComment: string;
+}
