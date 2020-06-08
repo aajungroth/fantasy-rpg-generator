adb1992e66c0e1a267c387815737a8d8fe83e5e1
aajungroth <aajungroth@ucdavis.edu>
2020-06-07 17:25:36 -0700

    Add post ability dto

---
+++ src/character/dto/postAbility.dto.ts
@@ -0,0 +1,7 @@
+export class PostAbilityDto {
+  readonly 'id'         : number;
+  readonly 'name'       : string;
+  readonly 'characterID': number;
+  readonly 'classID'    : number;
+  readonly 'targetID'   : number;
+}
