0983a65a82d0aba91c23c7d07ea7af6e87515aa7
aajungroth <aajungroth@ucdavis.edu>
2020-06-07 17:26:49 -0700

    Add base put dto to hanlde all put requests from games

---
+++ src/dto/basePut.dto.ts
@@ -0,0 +1,8 @@
+export class BasePutDto {
+  readonly id: string;
+  readonly name: string;
+  readonly description: string;
+  readonly createdBy: string;
+  readonly lastChangedBy: string;
+  readonly internalComment: string;
+}
