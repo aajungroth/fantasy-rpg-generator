b524aad671d0372383e231a4823c756082884a43
aajungroth <aajungroth@ucdavis.edu>
2020-06-09 19:51:39 -0700

    Make the internal comment optional in the dto

--- src/dto/basePost.dto.ts
+++ src/dto/basePost.dto.ts
@@ -3,5 +3,5 @@ export class BasePostDto {
   readonly description: string;
   readonly createdBy: string;
   readonly lastChangedBy: string;
-  readonly internalComment: string;
+  readonly internalComment?: string | null;
 }
