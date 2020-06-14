9efc9828e6f93bc3bbbac6d97db31da6d52c2249
aajungroth <aajungroth@ucdavis.edu>
2020-06-14 08:49:18 -0700

    Update base dto with all of the needed fields

--- src/dto/base.dto.ts
+++ src/dto/base.dto.ts
@@ -1,4 +1,8 @@
 export class BaseDto {
-  readonly id  : number;
-  readonly name: string;
+  readonly id              : string;
+  readonly name            : string;
+  readonly description     : string;
+  readonly createdBy       : string;
+  readonly lastChangedBy   : string;
+  readonly internalComment?: string | null;
 }
