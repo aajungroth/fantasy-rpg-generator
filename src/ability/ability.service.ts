e857068a618f2973580578393af8c63ad4a3073f
aajungroth <aajungroth@ucdavis.edu>
2020-06-15 19:03:37 -0700

    Write find all abilities and find one ability methods

--- src/ability/ability.service.ts
+++ src/ability/ability.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+'use strict';
+
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+
+import { BaseDto } from '../dto/base.dto';
+
+import { BasePostDto } from '../dto/basePost.dto';
+
+import { AbilityDao } from '../ability.dao';
+
+import { Ability } from '../model/ability.entity';
 
 @Injectable()
-export class AbilityService {}
+export class AbilityService {
+
+  constructor(private readonly abilityDao: AbilityDao) {}
+
+  public findAllAbilities(): Promise<Ability[]> {
+    return this.abilityDao.findAllAbilities();
+  }
+
+  public findOneAbility(abilityID): Promise<Ability> {
+    return this.abilityDao.findOneAbility(abilityID);
+  }
+
+}
