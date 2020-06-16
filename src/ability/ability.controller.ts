64b7af745d64e3cefc541f44ba9d01d1dd0597f0
aajungroth <aajungroth@ucdavis.edu>
2020-06-15 19:03:22 -0700

    Write ability controller

--- src/ability/ability.controller.ts
+++ src/ability/ability.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+'use strict';
+
+import { Controller, Get, Param } from '@nestjs/common';
+
+import { BaseDto } from '../dto/base.dto';
+
+import { BasePostDto } from '../dto/basePost.dto';
+
+import { AbilityService } from './ability.service';
 
 @Controller('ability')
-export class AbilityController {}
+export class AbilityController {
+
+  constructor(private readonly abilityService: AbilityService) {}
+
+  // Get all abilities in the game
+  @Get()
+  public findAllAbilities() {
+    return this.abilityService.findAllAbilities();
+  }
+
+  // Get the requested character if it exists
+  @Get(':abilityID')
+  public findOneAbility(@Param() params) {
+    return this.abilityService.findOneCharacter(params.abilityID);
+  }
+
+}
