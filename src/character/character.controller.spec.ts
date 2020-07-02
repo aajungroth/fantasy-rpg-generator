9ef018788f66142b1037bd6cb477cdb3b9634536
aajungroth <aajungroth@ucdavis.edu>
2020-07-01 21:41:08 -0700

    Simplify for testing

--- src/character/character.controller.spec.ts
+++ src/character/character.controller.spec.ts
@@ -1,18 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CharacterController } from './character.controller';
+import { CharacterDao} from './character.dao';
+import { CharacterService } from './character.service';
 
 describe('Character Controller', () => {
-  let controller: CharacterController;
+
+  let characterController: CharacterController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CharacterController],
+      providers: [CharacterDao, CharacterService]
     }).compile();
 
-    controller = module.get<CharacterController>(CharacterController);
+    characterController = module.get<CharacterController>(CharacterController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(characterController).toBeDefined();
   });
+
 });
