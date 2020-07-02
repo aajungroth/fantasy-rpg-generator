ca86a66b94b89c2c689eb5f06c412bf7a0aa2600
aajungroth <aajungroth@ucdavis.edu>
2020-07-01 21:40:36 -0700

    Write end to end test for the ability api

--- test/ability.e2e-spec.ts
+++ test/ability.e2e-spec.ts
@@ -1,17 +1,24 @@
 'use strict';
 
 import * as request from 'supertest';
+
 import { Test } from '@nestjs/testing';
+
 import { AppModule } from '../src/app.module';
+
 import { AbilityService } from '../src/ability/ability.service';
+
 import { INestApplication } from '@nestjs/common';
 
 describe('Ability', () => {
 
+  const findAllAbilitiesResult = [{'id': '1', 'name': 'findAbilities'}];
+  const findAbilityResult      = {'id': '2', 'name': 'findAbility'};
+
   let app: INestApplication;
   let abilityService = {
-    findAllAbilities: () => [{'id': '1', 'name': 'findOneAbility'}],
-    findOneAbility: () => {'id': '2', 'name': 'findOneAbility'},
+    findAllAbilities: () => findAllAbilitiesResult,
+    findAbility: () => findAbilityResult,
   };
 
   beforeAll(async () => {
@@ -31,20 +38,16 @@ describe('Ability', () => {
       return request(app.getHttpServer())
         .get('/ability')
         .expect(200)
-        .expect({
-          data: abilityService.findAllAbilities(),
-        });
+        .expect(abilityService.findAllAbilities());
     });
   });
 
   describe('/GET ability/:abilityID', () => {
     it('should get one ability by ID', () => {
-      return request(ap.getHttpServer())
+      return request(app.getHttpServer())
         .get('/ability/1')
         .expect(200)
-        .expect({
-          data: abilityService.findOneAbility(),
-        })
+        .expect(abilityService.findAbility())
     });
   });
 
