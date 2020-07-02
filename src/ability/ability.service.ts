3e90ae500f0037af469833ad38eaa8c27ff3d5e3
aajungroth <aajungroth@ucdavis.edu>
2020-07-01 21:38:37 -0700

    Write ability service

--- src/ability/ability.service.ts
+++ src/ability/ability.service.ts
@@ -6,7 +6,7 @@ import { BaseDto } from '../dto/base.dto';
 
 import { BasePostDto } from '../dto/basePost.dto';
 
-import { AbilityDao } from '../ability.dao';
+import { AbilityDao } from './ability.dao';
 
 import { Ability } from '../model/ability.entity';
 
@@ -15,12 +15,56 @@ export class AbilityService {
 
   constructor(private readonly abilityDao: AbilityDao) {}
 
-  public findAllAbilities(): Promise<Ability[]> {
+  // Get all abilities in the game
+  public findAllAbilities(): Promise<BaseDto[]> {
     return this.abilityDao.findAllAbilities();
   }
 
-  public findOneAbility(abilityID): Promise<Ability> {
-    return this.abilityDao.findOneAbility(abilityID);
+  // Get one ability by ID
+  public findAbility(abilityID: string): Promise<BaseDto> {
+    return this.abilityDao.findAbility(abilityID);
+  }
+
+  // Insert multiple abilities at once
+  public insertMultipleAbilities(abilityList: BasePostDto[]): Promise<any> {
+    return this.abilityDao.insertMultipleAbilities(abilityList);
+  }
+
+  // Insert a single ability
+  public insertAbility(abilityInfo: BasePostDto): Promise<any> {
+    return this.abilityDao.insertAbility(abilityInfo);
+  }
+
+  // Update multiple abilitis at once
+  public updateMultipleAbilities(abilityList: BaseDto[]): Promise<any[]> {
+    let result;
+
+    abilityList.forEach((abilityInfo: BaseDto) => {
+      result = this.abilityDao.updateAbility(abilityInfo);
+    });
+
+    return result;
+  }
+
+  // Update a single ability
+  public updateAbility(abilityInfo: BaseDto): Promise<any> {
+    return this.abilityDao.updateAbility(abilityInfo);
+  }
+
+  // Delete multiple abilities
+  public deleteMultipleAbilities(abilityList: BaseDto[]): Promise<any[]> {
+    let result;
+
+    abilityList.forEach((abilityInfo: BaseDto) => {
+      result = this.abilityDao.deleteAbility(abilityInfo);
+    });
+
+    return result;
+  }
+
+  // Delete a single ability
+  public deleteAbility(abilityInfo: BaseDto): Promise<any> {
+    return this.abilityDao.deleteAbility(abilityInfo);
   }
 
 }
