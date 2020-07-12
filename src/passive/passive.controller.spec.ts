bb2fe82389eca3cf8cfad4ebbbae8a375e88455c
aajungroth <aajungroth@ucdavis.edu>
2020-07-11 19:47:03 -0700

    Write passive controller spec

--- src/passive/passive.controller.spec.ts
+++ src/passive/passive.controller.spec.ts
@@ -1,18 +1,63 @@
+'use strict';
+
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { PassiveController } from './passive.controller';
 
+import { PassiveDao } from './passive.dao';
+
+import { PassiveService } from './passive.service';
+
+import { BaseDto } from '../dto/base.dto'
+
 describe('Passive Controller', () => {
-  let controller: PassiveController;
+
+  let passiveController: PassiveController;
+  let passiveDao: PassiveDao;
+  let passiveService: PassiveService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [PassiveController],
-    }).compile();
-
-    controller = module.get<PassiveController>(PassiveController);
+    passiveDao        = new PassiveDao();
+    passiveService    = new PassiveService();
+    passiveController = new PassiveController();
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(passiveController).toBeDefined();
   });
+
+  describe('findAllPassives', () => {
+    it('should return an array of abilities', async () => {
+      const result: BaseDto[] = [{
+        'id'           : '11',
+        'name'         : 'findAllPassives',
+        'description'  : 'A test',
+        'createdBy'    : 'spec',
+        'lastChangedBy': 'spec'
+      }];
+
+      jest.spyOn(passiveService, 'findAllPassives').mockImplementation(() => Promise.resolve(result));
+
+      expect(await passiveController.findAllPassives).toBe(result);
+    });
+  });
+
+  describe('findPassive', () => {
+    it('should return a single passive by id', async () => {
+      const result: BaseDto = {
+        'id'           : '12',
+        'name'         : 'findPassive',
+        'description'  : 'A test',
+        'createdBy'    : 'spec',
+        'lastChangedBy': 'spec'
+      };
+
+      const id: string = '12';
+
+      jest.spyOn(passiveService, 'findPassive').mockImplementation(() => Promsie.resolve(result));
+
+      expect(await passiveController.findPassive(id)).toBe(result);
+    });
+  });
+
 });
