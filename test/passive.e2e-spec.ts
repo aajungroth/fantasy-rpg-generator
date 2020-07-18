9c0c8c460e797c29391aa9a4dce1b52f232a55c4
aajungroth <aajungroth@ucdavis.edu>
2020-07-18 15:51:13 -0700

    Write passive end to end test

---
+++ test/passive.e2e-spec.ts
@@ -0,0 +1,58 @@
+'use strict';
+
+import * as request from 'supertest';
+
+import { Test } from '@nestjs/testing';
+
+import { AppModule } from '../src/app.module';
+
+import { AbilityService } from '../src/ability/ability.service';
+
+import { INestApplication } from '@nestjs/common';
+
+describe('Passive', ()  => {
+
+  const findAllPassivesResult = [{'id': '1', 'name': 'findPassives'}];
+  const findPassiveResult     = {'id': 2, 'name': 'findPassive'};
+
+  let app: INestApplication;
+  let abilityService = {
+    findAllPassive: () => findPassiveResult,
+    findPassive: () => findPassiveResult,
+  };
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(PassiveResult)
+      .userValue(passiveResult)
+      .compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  describe('/GET passive', () => {
+    it('should get all passives', () => {
+      return request(app.getHttpServer())
+        .get('/passive')
+        .expect(200)
+        .expect(passiveService.findAllPasives());
+    });
+  });
+
+  describe('/Get passive/:passiveID', () => {
+    it('should get one passive by ID', () => {
+      return request(app.getHttpServer())
+        .get('/passive/1')
+        .expect(200)
+        .expect(passiveService.findPassive())
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+});
