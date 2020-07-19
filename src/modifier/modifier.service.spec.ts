2cb402f30ff75a372542c65a38cb1211e8c27537
aajungroth <aajungroth@ucdavis.edu>
2020-07-19 14:35:06 -0700

    Write find all and find one specs for modifier service

--- src/modifier/modifier.service.spec.ts
+++ src/modifier/modifier.service.spec.ts
@@ -1,18 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { ModifierDao } from './modifier.dao';
+
 import { ModifierService } from './modifier.service';
 
+import { BaseDto } from '../dto/base.dto';
+
+import { BasePostDto } from '../dto/basePost.dto';
+
 describe('ModifierService', () => {
-  let service: ModifierService;
+
+  let modifierDao: ModifierDao;
+  let modifierService: ModifierService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ModifierService],
-    }).compile();
-
-    service = module.get<ModifierService>(ModifierService);
+    modifierDao     = new ModifierDao();
+    modifierService = new ModifierService(modifierDao);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(modifierService).toBeDefined();
   });
+
+  describe('findAllModifiers', () => {
+    it('should return an array of modifiers', async() => {
+      const result: BaseDto[] = [{
+        'id'           : '24',
+        'name'         : 'findAllModifiers',
+        'description'  : 'A test',
+        'createdBy'    : 'spec',
+        'lastChangedBy': 'spec'
+      }];
+
+      jest
+        .spyOn(modifierDao, 'findAllModifiers')
+        .mockImplementation(() => Promise.resolve(result));
+
+      expect(await modifierService.findAllModifiers()).toBe(result);
+    });
+  });
+
+  describe('findModifier', () => {
+    it('should return a single modifier by id', async () => {
+      const result: BaseDto = {
+        'id'           : '25',
+        'name'         : 'findModifier',
+        'description'  : 'A test',
+        'createdBy'    : 'spec',
+        'lastChangedBy': 'spec'
+      };
+
+      const id: string = '25';
+
+      jest
+        .spyOn(modifierDao, 'findModifier')
+        .mockImplementation(() => Promise.resolve(result));
+
+      expect(await modifierService.findModifier(id)).toBe(result);
+    });
+  });
+
 });
