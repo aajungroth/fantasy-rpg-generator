7cb482e36b2d7dce3c6f4eb07d6116ebef9586bb
aajungroth <aajungroth@ucdavis.edu>
2020-07-18 17:11:40 -0700

    Replaces instances of ability with passive

--- src/passive/passive.controller.ts
+++ src/passive/passive.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+'use strict';
+
+import { Controller, Get, Param } from '@nestjs/common';
+
+import { BaseDto } from '../dto/base.dto';
+
+import { PassiveService } from './passive.service';
 
 @Controller('passive')
-export class PassiveController {}
+export class PassiveController {
+
+  constructor(private readonly passiveService: PassiveService) {}
+
+  // Get all passives in the game
+  @Get()
+  public findAllPassives(): Promise<BaseDto[]> {
+    return this.passiveService.findAllPassives();
+  }
+
+  // Get on passive by ID
+  @Get(':passiveID')
+  public this.passiveService.findPassive(params.passiveID);
+
+}
