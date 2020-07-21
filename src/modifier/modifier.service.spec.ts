0e88bf0c51e91af24f5418537503f1b2907b01d1
aajungroth <aajungroth@ucdavis.edu>
2020-07-20 19:56:39 -0700

    Add specs for insert methods for modifier service

--- src/modifier/modifier.service.spec.ts
+++ src/modifier/modifier.service.spec.ts
@@ -25,7 +25,7 @@ describe('ModifierService', () => {
   describe('findAllModifiers', () => {
     it('should return an array of modifiers', async() => {
       const result: BaseDto[] = [{
-        'id'           : '24',
+        'id'           : '23',
         'name'         : 'findAllModifiers',
         'description'  : 'A test',
         'createdBy'    : 'spec',
@@ -43,20 +43,64 @@ describe('ModifierService', () => {
   describe('findModifier', () => {
     it('should return a single modifier by id', async () => {
       const result: BaseDto = {
-        'id'           : '25',
+        'id'           : '24',
         'name'         : 'findModifier',
         'description'  : 'A test',
         'createdBy'    : 'spec',
         'lastChangedBy': 'spec'
       };
 
-      const id: string = '25';
+      const id: string = '24';
 
       jest
         .spyOn(modifierDao, 'findModifier')
         .mockImplementation(() => Promise.resolve(result));
 
       expect(await modifierService.findModifier(id)).toBe(result);
+    });
+  });
+
+  describe('insertMultipleModifiers', () => {
+    it('should insert an array of modifiers', async () => {
+      const result: any[] = [{
+        'id'  : '25',
+        'name': 'insertMultipleModifiers'
+      }];
+
+      const input: BasePostDto[] = [{
+        'name'         : 'insertMultipleModifiers',
+        'description'  : 'A test',
+        'createdBy'    : 'spec',
+        'lastChangedBy': 'spec'
+      }];
+
+      jest
+        .spyOn(modifierDao, 'insertMultipleModifiers')
+        .mockImplementation(() => Promise.resolve(result));
+
+      expect(await modifierService.insertMultipleModifiers(input)).toBe(result);
+    });
+  });
+
+  describe('insertModifier', () => {
+    it('should insert a single modifier', async () => {
+      const result: any = {
+        'id'  : '26',
+        'name': 'insertModifier'
+      };
+
+      const input: BasePostDto = {
+        'name'         : 'insertModifier',
+        'description'  : 'A test',
+        'createdBy'    : 'spec',
+        'lastChangedBy': 'spec',
+      };
+
+      jest
+        .spyOn(modifierDao, 'insertModifier')
+        .mockImplementation(() => Promise.resolve(result));
+
+      expect(await modifierService.insertModifier(input)).toBe(result);
     });
   });
 
