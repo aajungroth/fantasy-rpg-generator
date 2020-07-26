cde68a7332940cb98bb60444e19660cc7826166f
aajungroth <aajungroth@ucdavis.edu>
2020-07-25 18:42:31 -0700

    Write controller for the modifier API

--- src/modifier/modifier.controller.ts
+++ src/modifier/modifier.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+'use strict';
+
+import { Controller, Get, Param } from '@nestjs/common';
+
+import { BaseDto } from '../dto/base.dto';
+
+import { ModifierService} from './modifier.service';
 
 @Controller('modifier')
-export class ModifierController {}
+export class ModifierController {
+
+  constructor(private readonly modifierService: ModifierService) {}
+
+  // Get all modifiers in the game
+  @Get()
+  public findAllModifiers(): Promise<BaseDto[]> {
+    return this.modifierService.findAllModifiers();
+  }
+
+  // Get one modifier by ID
+  @Get(':modifierID')
+  public findModifier(@Param() params): Promise<BaseDto> {
+    return this.modifierService.findModifier(params.modifierID);
+  }
+
+}
