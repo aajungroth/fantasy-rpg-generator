56f4e9711fc6720180b55bbb318eb36472473c79
aajungroth <aajungroth@ucdavis.edu>
2020-08-09 17:03:42 -0700

    Update model with many to one relationship to game table

--- src/model/character.entity.ts
+++ src/model/character.entity.ts
@@ -1,11 +1,13 @@
-import { Entity, Column, ManyToMany, JoinTable } from 'typeorm';
+import { Entity, Column, ManyToMany, ManyToOne, JoinTable } from 'typeorm';
 import { BaseEntity } from './base.entity';
 import { Ancestry } from './ancestry.entity';
 import { Background } from './background.entity';
 import { Class } from './class.entity';
+import { Game } from './game.entity';
 
 @Entity({ name: 'character' })
 export class Character extends BaseEntity {
+
   @ManyToMany(type => Ancestry)
   @JoinTable()
   abilities: Ancestry[];
@@ -17,4 +19,8 @@ export class Character extends BaseEntity {
   @ManyToMany(type => Class)
   @JoinTable()
   classes: Class[];
+
+  @ManyToOne(type => Game, (game) => game.id, {nullable: false})
+  public game!: Game;
+
 }
