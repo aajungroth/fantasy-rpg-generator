78f2d60c3234025dbcd9b2a0489b74032d927827
aajungroth <aajungroth@ucdavis.edu>
2020-08-09 17:12:58 -0700

    Update the model with the many to one relationship to the game table

--- src/model/passive.entity.ts
+++ src/model/passive.entity.ts
@@ -1,5 +1,11 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, ManyToOne } from 'typeorm';
 import { BaseEntity } from './base.entity';
+import { Game } from './game.entity';
 
 @Entity({ name: 'passive' })
-export class Passive extends BaseEntity {}
+export class Passive extends BaseEntity {
+
+  @ManyToOne(type => Game, (game) => game.id, {nullable: false})
+  public game!: Game;
+
+}
