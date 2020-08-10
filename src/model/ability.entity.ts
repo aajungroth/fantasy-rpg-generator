c9c37539ef6fffa914b8bae146db02627a08a481
aajungroth <aajungroth@ucdavis.edu>
2020-08-09 17:01:29 -0700

    Add many to one relationship to game table

--- src/model/ability.entity.ts
+++ src/model/ability.entity.ts
@@ -1,5 +1,11 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, ManyToOne } from 'typeorm';
 import { BaseEntity } from './base.entity';
+import { Game } from './game.entity';
 
 @Entity({ name: 'ability' })
-export class Ability extends BaseEntity {}
+export class Ability extends BaseEntity {
+
+  @ManyToOne(type => Game, (game) => game.id, {nullable: false})
+  public game!: Game;
+
+}
