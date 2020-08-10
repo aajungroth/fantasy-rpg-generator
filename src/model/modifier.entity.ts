1481187b6f8138b79e53d510054a72fd35898c0a
aajungroth <aajungroth@ucdavis.edu>
2020-08-09 17:12:19 -0700

    Update the model with a many to one relationship to the game table

--- src/model/modifier.entity.ts
+++ src/model/modifier.entity.ts
@@ -1,5 +1,11 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, ManyToOne } from 'typeorm';
 import { BaseEntity } from './base.entity';
+import { Game } from './game.entity';
 
 @Entity({ name: 'modifier' })
-export class Modifier extends BaseEntity {}
+export class Modifier extends BaseEntity {
+
+  @ManyToOne(type => Game, (game) => game.id, {nullable: false})
+  public game!: Game;
+
+}
