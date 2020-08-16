49f5488d4b8658bf068e04d4f3d00da99fc50f15
aajungroth <aajungroth@ucdavis.edu>
2020-08-16 16:11:12 -0700

    Write spec for game service

---
+++ src/game/game.service.spec.ts
@@ -0,0 +1,85 @@
+'use strict';
+
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { GameDao } from  './game.dao';
+
+import { GameService } from './game.service';
+
+import { BaseDto } from '../dto/base.dto';
+
+import { BasePostDto } from '../dto/basePost.dto';
+
+describe('GameService', () => {
+
+  let gameDao: GameDao;
+  let gameService: GameService;
+
+  beforeEach(async () => {
+    gameDao     = new GameDao();
+    gameService = new GameService(gameDao);
+  });
+
+  it('should be defined', () => {
+    expect(gameService).toBeDefined();
+  });
+
+  describe('findAllGames', () => {
+    it('should return an array of games', async() => {
+      const result: BaseDto[] = [{
+        'id'           : '34',
+        'name'         : 'findAllGames',
+        'description'  : 'A test',
+        'createdBy'    : 'spec',
+        'lastChangedBy': 'spec'
+      }];
+
+      jest
+        .spyOn(gameDao, 'findAllGames')
+        .mockImplementation(() => Promise.resolve(result));
+
+      expect(await gameService.findAllGames()).toBe(result);
+   });
+  });
+
+  describe('findGameByID', () => {
+    it('should return a game by id', async() => {
+      const result: BaseDto = {
+        'id'           : '35',
+        'name'         : 'findGameByID',
+        'description'  : 'A test',
+        'createdBy'    : 'spec',
+        'lastChangedBy': 'spec'
+      };
+
+      const id: string = '35';
+
+      jest
+        .spyOn(gameDao, 'findGameByID')
+        .mockImplementation(() => Promise.resolve(result));
+
+      expect(await gameService.findGameByID(id)).toBe(result);
+    });
+  });
+
+  describe('findGameByName', () => {
+    it('should return a game by name', async() => {
+      const result: BaseDto = {
+        'id'           : '36',
+        'name'         : 'findGameByName',
+        'description'  : 'A test',
+        'createdBy'    : 'spec',
+        'lastChangedBy': 'spec'
+      };
+
+      const name: string = 'findGameByName';
+
+      jest
+        .spyOn(gameDao, 'findGameByName')
+        .mockImplementation(() => Promise.resolve(result));
+
+      expect(await gameService.findGameByName(name)).toBe(result);
+    });
+  });
+
+});
