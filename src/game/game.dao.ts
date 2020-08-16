421a264e23ac5d04101dd86bbca121d59fc888b1
aajungroth <aajungroth@ucdavis.edu>
2020-08-16 16:10:05 -0700

    Write game dao

---
+++ src/game/game.dao.ts
@@ -0,0 +1,43 @@
+'use strict';
+
+import { Injectable } from '@nestjs/common';
+
+import { getConnection } from 'typeorm';
+
+import { Game } from '../model/game.entity';
+
+import { BaseDto } from '../dto/Base.dto';
+
+@Injectable()
+export class GameDao {
+
+  // Retrieves all of the games
+  public async findAllGames(): Promise<BaseDto[]> {
+    return await getConnection()
+      .createQueryBuilder()
+      .select('game')
+      .from(Game, 'game')
+      .getMany();
+  }
+
+  // Retrieves a single game by ID
+  public async findGameByID(gameID): Promise<BaseDto> {
+    return await getConnection()
+      .createQueryBuilder()
+      .select('game')
+      .from(Game, 'game')
+      .where('game.id = :id', { id: gameID })
+      .getOne();
+  }
+
+  // Retrieves a single game by name
+  public async  findGameByName(gameName): Promise<BaseDto> {
+    return await getConnection()
+      .createQueryBuilder()
+      .select('game')
+      .from(Game, 'game')
+      .where('game.name = :game', { name: gameName })
+      .getOne();
+  }
+
+}
