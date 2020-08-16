3fbe8f0db50b226d18215ba5a8fd38b53b5afad6
aajungroth <aajungroth@ucdavis.edu>
2020-08-16 16:06:29 -0700

    Add done and adjust spacing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,3 +1,5 @@
+'use strict';
+
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
@@ -6,21 +8,24 @@ describe('AppController (e2e)', () => {
   let app;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    const moduleFixture: TestingModule = await Test
+      .createTestingModule({
+        imports: [AppModule],
+      })
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
   describe('app', () => {
-    it('/ (GET)', () => {
+    it('/GET', (done) => {
       return request(app.getHttpServer())
         .get('/')
         .expect(200)
-        .expect('Hello World!');
-    })
+        .expect('Hello World!')
+        .end(done);
+    });
   });
 
   afterAll(async () => {
