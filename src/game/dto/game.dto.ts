3d597eed5abc4662462efc265ae70f16a04ed066
aajungroth <aajungroth@ucdavis.edu>
2020-08-31 17:32:39 -0700

    Add dto for the game API

---
+++ src/game/dto/game.dto.ts
@@ -0,0 +1,8 @@
+export class GameDto {
+  readonly id              : string;
+  readonly name            : string;
+  readonly description     : string;
+  readonly createdBy       : string;
+  readonly lastChangedBy   : string;
+  readonly internalComment?: string | null;
+}
