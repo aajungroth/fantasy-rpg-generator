169efcd17900c0f3a9cc1abe48b05fa468c7eed7
aajungroth <aajungroth@ucdavis.edu>
2020-09-01 13:21:45 -0700

    Add gameId

--- src/model/character.entity.ts
+++ src/model/character.entity.ts
@@ -21,6 +21,6 @@ export class Character extends BaseEntity {
   classes: Class[];
 
   @ManyToOne(type => Game, (game) => game.id, {nullable: false})
-  public game!: Game;
+  public gameId!: Game['id'];
 
 }
