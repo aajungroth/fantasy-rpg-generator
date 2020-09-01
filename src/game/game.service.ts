df035c2d3a8431f608404c63fb61d14641fe3dce
aajungroth <aajungroth@ucdavis.edu>
2020-08-31 17:38:53 -0700

    Update to game dto and Id

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -2,9 +2,9 @@
 
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
-import { BaseDto } from '../dto/base.dto';
+import { GameDto } from './dto/game.dto';
 
-import { BasePostDto } from '../dto/basePost.dto';
+import { GamePostDto } from './dto/gamePost.dto';
 
 import { GameDao } from './game.dao';
 
@@ -14,17 +14,17 @@ export class GameService {
   constructor(private readonly gameDao: GameDao) {}
 
   // Get all games
-  public findAllGames(): Promise<BaseDto[]> {
+  public findAllGames(): Promise<GameDto[]> {
     return this.gameDao.findAllGames();
   }
 
-  // Get one game by ID
-  public findGameByID(gameID: string): Promise<BaseDto> {
-    return this.gameDao.findGameByID(gameID);
+  // Get one game by Id
+  public findGameById(gameId: string): Promise<GameDto> {
+    return this.gameDao.findGameById(gameId);
   }
 
   // Get a list of games by name
-  public findGameListByName(gameName: string): Promise<BaseDto[]> {
+  public findGameListByName(gameName: string): Promise<GameDto[]> {
     return this.gameDao.findGameListByName(gameName);
   }
 
