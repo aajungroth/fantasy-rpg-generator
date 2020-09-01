099e4d1a4f702da8643c0a2d5cc3023315628cb1
aajungroth <aajungroth@ucdavis.edu>
2020-09-01 13:22:43 -0700

    Add gameId

--- src/model/passive.entity.ts
+++ src/model/passive.entity.ts
@@ -6,6 +6,6 @@ import { Game } from './game.entity';
 export class Passive extends BaseEntity {
 
   @ManyToOne(type => Game, (game) => game.id, {nullable: false})
-  public game!: Game;
+  public gameId!: Game['id'];
 
 }
