ae645e17323210e6c23b083767081c5badd7285e
aajungroth <aajungroth@ucdavis.edu>
2020-08-31 17:38:15 -0700

    Update to game dto and Id

--- src/game/game.dao.ts
+++ src/game/game.dao.ts
@@ -6,13 +6,13 @@ import { getConnection } from 'typeorm';
 
 import { Game } from '../model/game.entity';
 
-import { BaseDto } from '../dto/Base.dto';
+import { GameDto } from './dto/Game.dto';
 
 @Injectable()
 export class GameDao {
 
   // Retrieves all of the games
-  public async findAllGames(): Promise<BaseDto[]> {
+  public async findAllGames(): Promise<GameDto[]> {
     return await getConnection()
       .createQueryBuilder()
       .select('game')
@@ -20,18 +20,18 @@ export class GameDao {
       .getMany();
   }
 
-  // Retrieves a single game by ID
-  public async findGameByID(gameID): Promise<BaseDto> {
+  // Retrieves a single game by Id
+  public async findGameById(gameId): Promise<GameDto> {
     return await getConnection()
       .createQueryBuilder()
       .select('game')
       .from(Game, 'game')
-      .where('game.id = :id', { id: gameID })
+      .where('game.id = :id', { id: gameId })
       .getOne();
   }
 
   // Retrieves a list of games by name
-  public async findGameListByName(gameName): Promise<BaseDto[]> {
+  public async findGameListByName(gameName): Promise<GameDto[]> {
     return await getConnection()
       .createQueryBuilder()
       .select('game')
