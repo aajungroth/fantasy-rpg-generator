3d7e1785c464159975282bb719b93e28f6fbe4ef
aajungroth <aajungroth@ucdavis.edu>
2020-09-02 16:10:04 -0700

    Add foreign key to game public key table

--- src/model/game.entity.ts
+++ src/model/game.entity.ts
@@ -1,5 +1,12 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, OneToOne, JoinColumn } from 'typeorm';
 import { BaseEntity } from './base.entity';
+import { GamePublicKey } from './gamePublicKey.entity';
 
 @Entity({ name: 'game' })
-export class Game extends BaseEntity {}
+export class Game extends BaseEntity {
+
+  @OneToOne(type => GamePublicKey, (gamePublicKey) => gamePublicKey.id, {nullable: false})
+  @JoinColumn({ name: 'gamePublicKeyId' })
+  public gamePublicKeyId!: GamePublicKey['id'];
+
+}
